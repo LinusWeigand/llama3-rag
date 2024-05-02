96420364eb6a932249751ac58876a14708249b49
Linus Weigand <linusweigand@protonmail.com>
2024-05-02 20:22:26 +0200

    comment out grading

--- simple.py
+++ simple.py
@@ -23,9 +23,26 @@ vectorstore = Chroma.from_documents(
 )
 retriever = vectorstore.as_retriever()
 
+# GRADE
+from langchain.prompts import PromptTemplate
+
+# prompt = PromptTemplate(
+#     template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing relevance 
+#     of a retrieved document to a user question. If the document contains keywords related to the user question, 
+#     grade it as relevant. It does not need to be a stringent test. The goal is to filter out erroneous retrievals. \n
+#     Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question. \n
+#     Provide the binary score as a JSON with a single key 'score' and no premable or explaination.
+#      <|eot_id|><|start_header_id|>user<|end_header_id|>
+#     Here is the retrieved document: \n\n {document} \n\n
+#     Here is the user question: {question} \n <|eot_id|><|start_header_id|>assistant<|end_header_id|>
+#     """,
+#     input_variables=["question", "document"],
+# )
+# retrieval_grader = prompt | llm | JsonOutputParser()
+
+
 # GENERATE
 from langchain_core.output_parsers import StrOutputParser
-from langchain.prompts import PromptTemplate
 from langchain_community.chat_models import ChatOllama
 
 prompt = PromptTemplate(
@@ -57,6 +74,31 @@ def retrieve(state):
     question = state["question"]
     documents = retriever.invoke(question)
     return {"documents": documents, "question": question}
+
+# def grade_documents(state):
+#     print("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
+#     question = state["question"]
+#     documents = state["documents"]
+#     
+#     # Score each doc
+#     filtered_docs = []
+#     web_search = "No"
+#     for d in documents:
+#         score = retrieval_grader.invoke({"question": question, "document": d.page_content})
+#         grade = score['score']
+#         # Document relevant
+#         if grade.lower() == "yes":
+#             print("---GRADE: DOCUMENT RELEVANT---")
+#             filtered_docs.append(d)
+#         # Document not relevant
+#         else:
+#             print("---GRADE: DOCUMENT NOT RELEVANT---")
+#             # We do not include the document in filtered_docs
+#             # We set a flag to indicate that we want to run web search
+#             web_search = "Yes"
+#             continue
+#     return {"documents": filtered_docs, "question": question, "web_search": web_search}
+    
 
 def generate(state):
     print("---GENERATE---")
